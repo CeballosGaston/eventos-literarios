2373caaa060bd9a5f58d6f4997e3981e3a1dd644
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-04-21 15:22:17 +0200

    fix: clean the comments on proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -4,8 +4,6 @@ import { NextResponse, type NextRequest } from "next/server";
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  // 1. EXCLUSIÓN MANUAL INMEDIATA
-  // Si es login, register o un archivo estático, NI SIQUIERA inicializamos Supabase.
   if (pathname.includes(".") || pathname.startsWith("/_next")) {
     return NextResponse.next();
   }
@@ -44,28 +42,23 @@ export async function proxy(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  // 1. Definimos cuáles son las páginas de autenticación
   const isAuthPage = pathname === "/login" || pathname === "/register";
 
-  // CASO A: Usuario NO logueado intentando entrar a zona privada
   if (!user && !isAuthPage) {
     const url = request.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.redirect(url);
   }
 
-  // CASO B: Usuario SÍ logueado intentando entrar a Login o Register (LO QUE BUSCAMOS)
   if (user && isAuthPage) {
     const url = request.nextUrl.clone();
-    url.pathname = "/"; // O a /dashboard si prefieres
+    url.pathname = "/";
     return NextResponse.redirect(url);
   }
 
-  // Si no cae en ninguna de las anteriores, lo dejamos pasar
   return response;
 }
 
 export const config = {
-  // Matcher ultra-simple para que no interfiera con nada más
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
