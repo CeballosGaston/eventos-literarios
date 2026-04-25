dec52f2d64a8668c3f9b57cbabf86459fa2a8e94
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-04-25 21:16:03 +0200

    feat: events page added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,24 +1,9 @@
 "use client";
 
-import { useEvents } from "@/features/events/hooks/useEvents";
-import { useFilters } from "../../features/filters/context/FiltersContext";
-
 export default function DashboardPage() {
-  const { filters } = useFilters();
-  const { data: events } = useEvents(filters);
-
   return (
     <div>
       <h1>Home</h1>
-
-      {events?.map((e) => (
-        <div key={e.id}>
-          <p>{e.title}</p>
-          <p>
-            {e.start_date} - {e.end_date}
-          </p>
-        </div>
-      ))}
     </div>
   );
 }
